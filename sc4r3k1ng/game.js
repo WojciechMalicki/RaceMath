e3f77ed4f5eeee03fd84778bf9337ec4488f9d5e
Sc4r3k1ng <46623448+Sc4r3k1ng@users.noreply.github.com>
2019-01-12 14:51:00 +0100

    Create game.js

---
+++ sc4r3k1ng/game.js
@@ -0,0 +1,12 @@
+function newGame()
+{
+var images = ['a1.jpg', 'I2.png', 'I3.png', 'I4.png', 'I5.png'];
+var answer = [1, 2, 3, 4, 5];
+var score = 0;
+var gameTime = 100;
+var baseURL = './zzp/';
+
+	display = '<img scr="' + baseURL + images[0] + '"/>';
+	document.getElementById("question").innerHTML = display;
+	console.log(display);
+}
