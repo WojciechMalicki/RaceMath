076327b33597f2bc5281b2de45fe7eaa85be75c4
damian12344 <46623417+damian12344@users.noreply.github.com>
2019-01-12 14:52:35 +0100

    Create gra.js

---
+++ damian12344/gra.js
@@ -0,0 +1,10 @@
+function newGame()
+{
+ var images = ['i1.jpg', 'i2.jpg', 'i3.jpg', 'i4.jpg', 'i5.jpg'];
+ var answer = [1, 2, 3, 4, 5];	    
+ var score = 0;
+ var gameTime = 100;
+ var baseURL = './img/';
+ display = '<img src="' + baseURL + images[0] +'"/>';
+ document.getElementById("question").innerHTML = display; 
+}
